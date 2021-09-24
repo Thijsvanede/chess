493187f827a2d4eae210487b052f137ef3f4f43a
Thijs van Ede <t.s.vanede@utwente.nl>
2021-09-24 17:14:29 +0200

    Added basic board setup from FEN

--- chess/board.py
+++ chess/board.py
@@ -1,12 +1,32 @@
 import numpy as np
+import pieces
 
 class Board(object):
 
-    def __init__(self, width=8, height=8):
+    def __init__(self, n_ranks=8, n_files=8):
         """"""
-        self.width  = width
-        self.height = height
-        self.board  = np.zeros((width, height), dtype=object)
+        self.n_files   = n_files
+        self.n_ranks   = n_ranks
+        self.board     = np.zeros((n_ranks, n_files), dtype=object)
+        self.color     = 'w'
+        self.castling = 'KQkq'
+
+    ########################################################################
+    #                              Get moves                               #
+    ########################################################################
+
+    def moves(self, rank, file):
+        """"""
+        if self.board[rank, file] is None:
+            # Return no moves
+            return np.zeros((self.n_ranks, self.n_files), dtype=bool)
+        else:
+            # Return moves for piece
+            return self.board[rank, file].moves(
+                rank = rank,
+                file = file,
+                castling = self.castling,
+            )
 
     ########################################################################
     #                             I/O methods                              #
@@ -15,7 +35,63 @@ class Board(object):
     @classmethod
     def from_fen(cls, fen):
         """"""
-        board = Board()
+        # Parse FEN
+        position, color, castling, en_passant, halfmove, fullmove = fen.split()
+
+        # Parse position
+        setup = list()
+        # Loop over all ranks in FEN
+        for rank in position.split('/'):
+            # Start new rank
+            setup.append(list())
+
+            # Loop over each individual piece
+            for piece in rank:
+                # Get color from piece
+                if piece.islower():
+                    color = pieces.Color.BLACK
+                else:
+                    color = pieces.Color.WHITE
+
+                # Set specific piece
+                if piece.lower() == 'p':
+                    setup[-1].append(pieces.Pawn(color))
+                elif piece.lower() == 'n':
+                    setup[-1].append(pieces.Knight(color))
+                elif piece.lower() == 'b':
+                    setup[-1].append(pieces.Bishop(color))
+                elif piece.lower() == 'r':
+                    setup[-1].append(pieces.Rook(color))
+                elif piece.lower() == 'q':
+                    setup[-1].append(pieces.Queen(color))
+                elif piece.lower() == 'k':
+                    setup[-1].append(pieces.King(color))
+                else:
+                    for _ in range(int(piece)):
+                        setup[-1].append(None)
+
+        # Setup board as numpy array
+        setup = np.asarray(setup, dtype=object)
+
+        # Ensure we have 2 dimensions
+        assert setup.ndim == 2, "FEN notation differed per rank."
+
+        # Create board
+        board = cls(
+            n_files = setup.shape[0],
+            n_ranks = setup.shape[1],
+        )
+
+        # Setup board
+        board.board      = setup
+        board.color      = color
+        board.castling   = castling
+        board.en_passant = None if en_passant == '-' else en_passant
+        board.halfmove   = int(halfmove)
+        board.fullmove   = int(fullmove)
+
+        # Return result
+        return board
 
 
 
@@ -26,7 +102,7 @@ class Board(object):
     def __str__(self):
         """"""
         # Setup board top
-        result  = "╔" + "═══╤"*(self.width-1) + "═══╗\n"
+        result  = "╔" + "═══╤"*(self.n_files-1) + "═══╗\n"
 
         # Setup board rows
         for index_rank, row in enumerate(self.board):
@@ -35,11 +111,11 @@ class Board(object):
             for index_file, square in enumerate(row):
                 result += "{:^3}".format(str(square) if square else ' ')
 
-                if index_file == self.width-1 and index_rank != self.height-1:
-                    result += "║\n╟" + "───┼"*(self.width-1) + "───╢\n"
-                elif index_file == self.width-1 and index_rank == self.height-1:
+                if index_file == self.n_files-1 and index_rank != self.n_ranks-1:
+                    result += "║\n╟" + "───┼"*(self.n_files-1) + "───╢\n"
+                elif index_file == self.n_files-1 and index_rank == self.n_ranks-1:
                     # Setup board bottom
-                    result += "║\n╚" + "═══╧"*(self.width-1) + "═══╝"
+                    result += "║\n╚" + "═══╧"*(self.n_files-1) + "═══╝"
                 else:
                     result += '│'
 
@@ -47,5 +123,5 @@ class Board(object):
         return result
 
 if __name__ == "__main__":
-    board = Board()
+    board = Board.from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
     print(board)
